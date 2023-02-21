33e3ee48850b2ad9b6d27a2f55945b802309c9ed
Jefferson Davis <davisjd.jefferson23@gmail.com>
2023-02-21 18:51:14 +0200

    timer intervals working. need to add d ifferentiating between adults there with children and adults there for a cut

--- interruptors.cpp
+++ interruptors.cpp
@@ -21,19 +21,21 @@
 
 
 //Define outputs
-DigitalOut l1(c1);
-DigitalOut l2(c2);
-DigitalOut l3(c3);
-DigitalOut l4(c4);
-DigitalOut l5(c5);
-DigitalOut l6(c6);
-DigitalOut l7(c7);
-DigitalOut l8(c8);
+// DigitalOut l1(c1);
+// DigitalOut l2(c2);
+// DigitalOut l3(c3);
+// DigitalOut l4(c4);
+// DigitalOut l5(c5);
+// DigitalOut l6(c6);
+// DigitalOut l7(c7);
+// DigitalOut l8(c8);
 DigitalOut limit(lm);
 DigitalOut bb1(b1);
 DigitalOut bb2(b2);
 DigitalOut bb3(b3);
 DigitalOut clk(CLOCK);
+BusOut  ledin(c1, c2, c3, c4, c5, c6, c7, c8);
+BusOut  bab(b1, b2, b3);
 
  
 
@@ -46,49 +48,78 @@ InterruptIn fir(FIRE);
 
 //Define counters
 static volatile unsigned int count1;
-static volatile unsigned int countmax;
+static volatile unsigned int countmax = 8;
 static volatile unsigned int chldcount;
 static volatile unsigned int adcount;
+static volatile unsigned int barbers;
+// static volatile unsigned int barber2;
+// static volatile unsigned int barber3;
 
 
-void updateLEDs(){
-  l1 = 0;
-  l2 = 0;
-  l3 = 0;
-  l4 = 0;
-  l5 = 0;
-  l6 = 0;
-  l7 = 0;
-  l8 = 0;
-  countmax = 8;
-  
+void handle_barbers()
+{
+  int i;
+
+  i = 0;
+  printf("child %i ad %i \n", adcount, chldcount);
+  if ((!bb1 || !bb2) && adcount && adcount - chldcount > 0)
+  {
+    if (!bb1)
+    {
+      bb1 = 1;
+      --adcount;
+      ++barbers;
+        ++i;
+    }
+    else if (!bb2)
+    {
+      bb2 = 1;
+      --adcount;
+      ++barbers;
+      ++i;
+    }
+  }
+  if (!bb3 && chldcount)
+  {
+    bb3 = 1;
+    chldcount--;
+    ++barbers;
+    ++i;
+  }
+  count1 -= i;
+}
+
+void updateLEDs()
+{
+  handle_barbers();
   switch (count1)
   {
+      case 0:
+        ledin = 0b00000000;
+        break ;
       case 1:
-        l1 = 1;
+        ledin = 0b00000001;
         break ;
       case 2:
-            printf("here\n");
-        l1 = 1;
-        l2 = 1;
+        ledin = 0b00000011;
         break;
       case 3:
-        l1 = l2 = l3 = 1;
+        ledin = 0b00000111;
         break;
       case 4:
-        l1 = l2 = l3 = l4 = 1;
+       ledin = 0b00001111;
         break;
       case 5:
-        l1 = l2 = l3 = l4 = l5 = 1;
+        ledin = 0b00011111;
         break;
       case 6:
-        l1 = l2 = l3 = l4 = l5 = l6 = 1;
+        ledin = 0b00111111;
         break;
       case 7:
-        l1 = l2 = l3 = l4 = l5 = l6 = l7 = 1;
+        ledin = 0b01111111;
         break;
       case 8:
-        l1 = l2 = l3 = l4 = l5 = l6 = l7 = l8 = 1;
+        ledin = 0b11111111;
         break;
   }
 }
@@ -101,26 +132,30 @@ void adult_handler(){
 	{
 	    count1++;
 	    adcount++;
+	    printf("this count %i\n", count1);
 	}
 	updateLEDs();
 	//Write your code here
 	
 }
 
-void child_handler(){
+void child_handler()
+{
 	if (count1 < countmax - 1)
 	{
-	    chdcount++;
+	    chldcount++;
 	    adcount++;
 	    count1 += 2;
 	}
 	updateLEDs();
 	//Write your code here
-	
 }
 
 void fire_handler(){
 	count1 = 0;
+	bab = 0b000;
+    adcount = chldcount = 0;
+    barbers = 0;
 	updateLEDs();
 	//Write your code here
 	
@@ -135,6 +170,7 @@ int main(){
 		
 	//Initially turn off all LEDs and set all the counters to 0
     updateLEDs();
+    int i;
 
 	//Interrupt handlers
 	ad.rise(&adult_handler);
@@ -147,8 +183,20 @@ int main(){
 	//wait 100 ms
 	while(1)
 	{
+	    if (i == 24)
+	    {	            printf("barbers %i\n", barbers);
+
+	        bab = 0;
+	        barbers = 0;
+	        i = 0;
+	        clk = 1;
+	        wait_ms(100);
+	        updateLEDs();
+	        clk = 0;
+	    }
 	    printf("count = %i\n", count1);
-		wait_ms(1200);
+		wait_ms(100);
+		++i;
 		
 	}
 }
